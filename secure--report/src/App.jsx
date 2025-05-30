fe3972b984db2f2422235ac54d77ec4666daf1f4
chuchito24 <hjsalinas123@gmail.com>
2025-05-29 19:18:37 -0600

    avance 29-05

--- secure--report/src/App.jsx
+++ secure--report/src/App.jsx
@@ -1,16 +1,37 @@
 import './App.css';
-import logo from '/logopequeño.png'; // ruta desde carpeta public
+import logo from '/logopequeño.png'; 
 
 function App() {
   return (
-    <div className="main-container">
-      <div className="decor-left"></div>
-      <div className="decor-right"></div>
+    <div className="container">
+      <div className="header">
+        <button className="header-button">Iniciar sesión</button>
+        <button className="header-button">Crear cuenta</button>
+      </div>
 
       <div className="content">
-        <h1>"SECURE REPORT"</h1>
-        <img src={logo} alt="logo" className="logo" />
-        <button className="btn">BIENVENIDO</button>
+        <div className="left">
+          <h2>Bienvenido a Secure Report</h2>
+          <p>
+¿Perdiste algo por causa de la delincuencia? ¿Alguien cercano está desaparecido? Esta plataforma está diseñada para ayudarte. 
+Aquí podrás realizar reportes de manera rápida y sencilla, compartir información con tu comunidad y recibir alertas que pueden marcar la diferencia. 
+Nuestro objetivo es hacer que encontrar personas u objetos desaparecidos sea más fácil, conectando a los ciudadanos con las autoridades y entre sí. 
+Juntos, podemos construir un entorno más seguro y solidario.</p>
+        </div>
+
+        <div className="center">
+          <h1 className="sr-logo">SR</h1>
+        </div>
+
+        <div className="right">
+          <h2>Información</h2>
+          <p>Datos relevantes...</p>
+        </div>
+      </div>
+
+      <div className="footer">
+      
+        <img src="/imagen.png" alt="SR Logo" className="logo" />
       </div>
     </div>
   );
